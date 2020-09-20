bceae5e51053294b1328f16018ebd0aaf5c71f81
yoggaajipratama <yoggaajipratama99@gmail.com>
2020-09-20 15:35:54 +0700

    Grafik fix bug, detail

--- DB/pakkem_daftar.sql
+++ DB/pakkem_daftar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 19, 2020 at 12:17 PM
+-- Generation Time: Sep 20, 2020 at 03:35 PM
 -- Server version: 8.0.21-0ubuntu0.20.04.4
 -- PHP Version: 7.4.3
 
@@ -30,18 +30,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `anggota` (
   `id_pendaftar` int NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `instansi` varchar(100) NOT NULL,
-  `tempat_lahir` varchar(100) NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `instansi` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `tempat_lahir` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `tanggal_lahir` date NOT NULL,
-  `gender` varchar(50) NOT NULL,
-  `nik` char(16) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `no_hp` char(16) NOT NULL,
-  `bisa_wa` varchar(50) NOT NULL,
-  `pendidikan` varchar(255) NOT NULL,
-  `sertifikat` varchar(255) NOT NULL,
-  `info_tambahan` varchar(255) NOT NULL,
+  `gender` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `nik` char(16) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `no_hp` char(16) COLLATE utf8mb4_general_ci NOT NULL,
+  `bisa_wa` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `pendidikan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `sertifikat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `info_tambahan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `tgl_daftar` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -53,10 +53,18 @@ CREATE TABLE `anggota` (
 
 CREATE TABLE `foto` (
   `id` int NOT NULL,
-  `foto_upload` varchar(200) DEFAULT NULL,
-  `nama_pemilik` varchar(100) NOT NULL,
+  `foto_upload` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nama_pemilik` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email_pemilik` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
   `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `foto`
+--
+
+INSERT INTO `foto` (`id`, `foto_upload`, `nama_pemilik`, `email_pemilik`) VALUES
+(23, '261149545_yoggaajipratama@gmail.com.png', 'Yogga Aji Pratama', 'yoggaajipratama@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -66,21 +74,28 @@ CREATE TABLE `foto` (
 
 CREATE TABLE `pendaftar` (
   `id_pendaftar` int NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `instansi` varchar(100) NOT NULL,
-  `tempat_lahir` varchar(100) NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `instansi` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `tempat_lahir` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `tanggal_lahir` date NOT NULL,
-  `gender` varchar(50) NOT NULL,
-  `nik` char(16) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `no_hp` char(16) NOT NULL,
-  `bisa_wa` varchar(50) NOT NULL,
-  `pendidikan` varchar(255) NOT NULL,
-  `sertifikat` varchar(255) NOT NULL,
-  `info_tambahan` varchar(255) NOT NULL,
+  `gender` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `nik` char(16) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `no_hp` char(16) COLLATE utf8mb4_general_ci NOT NULL,
+  `bisa_wa` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `pendidikan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `sertifikat` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `info_tambahan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `tgl_daftar` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pendaftar`
+--
+
+INSERT INTO `pendaftar` (`id_pendaftar`, `nama`, `instansi`, `tempat_lahir`, `tanggal_lahir`, `gender`, `nik`, `email`, `no_hp`, `bisa_wa`, `pendidikan`, `sertifikat`, `info_tambahan`, `status`) VALUES
+(49, 'Yogga Aji Pratama', 'UIN Sunan Gunung Djati Bandung', 'Bandung', '2020-09-08', 'Laki-laki', '1010101010101010', 'yoggaajipratama@gmail.com', '10101010101010', 'Ya', 'D IV', 'Ya, ada', 'Tidak ada', 0);
 
 -- --------------------------------------------------------
 
@@ -90,11 +105,19 @@ CREATE TABLE `pendaftar` (
 
 CREATE TABLE `sertifikat` (
   `id` int NOT NULL,
-  `sertifikat_upload` varchar(200) DEFAULT NULL,
-  `nama_pemilik` varchar(100) NOT NULL,
-  `keterangan` varchar(100) NOT NULL,
+  `sertifikat_upload` varchar(200) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nama_pemilik` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email_pemilik` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `keterangan` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `waktu` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sertifikat`
+--
+
+INSERT INTO `sertifikat` (`id`, `sertifikat_upload`, `nama_pemilik`, `email_pemilik`, `keterangan`) VALUES
+(19, '261149545_yoggaajipratama@gmail.com.png', 'Yogga Aji Pratama', 'yoggaajipratama@gmail.com', 'Sertifikat UIN');
 
 -- --------------------------------------------------------
 
@@ -104,11 +127,11 @@ CREATE TABLE `sertifikat` (
 
 CREATE TABLE `users` (
   `id_user` int NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `level` varchar(50) NOT NULL,
+  `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `level` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `log` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -154,25 +177,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `id_pendaftar` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pendaftar` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `pendaftar`
 --
 ALTER TABLE `pendaftar`
-  MODIFY `id_pendaftar` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id_pendaftar` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `sertifikat`
 --
 ALTER TABLE `sertifikat`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `users`
