ae019d9b8d21068b964f13c1d92f66e509d7661a
Moh Ramdani Yusman <moh.ramdani22@gmail.com>
2020-09-22 16:03:01 +0700

    Fix Timestamp

--- admin/templates/tambah_aksi.php
+++ admin/templates/tambah_aksi.php
@@ -2,6 +2,7 @@
 <?php 
 // koneksi database
 include 'koneksi.php';
+global $koneksi;
  
 // menangkap data yang di kirim dari form
 $hari_tanggal = $_POST['hari_tanggal'];
@@ -9,14 +10,18 @@ $waktu_agenda = $_POST['waktu_agenda'];
 $kegiatan = $_POST['kegiatan'];
 $keterangan_agenda = $_POST['keterangan_agenda'];
  
+
+
+$query = "INSERT INTO `agenda_kegiatan` (`id_agenda`, `hari_tanggal`, `waktu_agenda`, `kegiatan`, `keterangan_agenda`, `waktu_upload`) VALUES (NULL, '$hari_tanggal', '$waktu_agenda', '$kegiatan', '$keterangan_agenda', CURRENT_TIMESTAMP)";
+
 // menginput data ke database
-mysqli_query($koneksi,"insert into agenda_kegiatan values('','$hari_tanggal','$waktu_agenda','$kegiatan','$keterangan_agenda','')");
- 
 // mengalihkan halaman kembali ke index.php
-header("location:agenda_kegiatan_upload.php");
+$sql = mysqli_query($koneksi, $query);
+	if($sql){
+		header("location:agenda_kegiatan_upload.php");
+	}
+
+ 
+
  
 ?>
-
-
-
-
