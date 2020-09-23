406103aa557e94e9af98f0fd1cc67ec02ecf7449
yoggaajipratama <yoggaajipratama99@gmail.com>
2020-09-23 21:48:01 +0700

    OK

--- admin/models/config_admin.php
+++ admin/models/config_admin.php
@@ -1,7 +1,6 @@
 <?php
 /* Attempt to connect to MySQL database */
 
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME); 
 $link = mysqli_connect("localhost", "pakkem", "Covid*20", "pakkem_pakkem");
 
 /* Check connection */
