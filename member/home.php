efeec3d314f9ffcce4a64979913a5efb797ea01f
yoggaajipratama <yoggaajipratama99@gmail.com>
2020-09-23 13:21:45 +0700

    OK

--- member/home.php
+++ member/home.php
@@ -6,7 +6,6 @@
     }
 ?>
 <?php require "base/navbar.php" ?>
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -26,7 +25,8 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Agenda</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Agenda Diikuti</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <p></p>
                             </div>
@@ -45,7 +45,8 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Modul</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Total Modul Baru</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <p></p>
                             </div>
@@ -65,25 +66,10 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Jadwal Agenda</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= jumlah(1); ?></div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fa fa-thumbs-down fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Pending Requests Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Agenda Ditunda
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= jumlah(2); ?></div>
+                                Total Agenda Masuk</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <p></p>
+                            </div>
                         </div>
                         <div class="col-auto">
                             <i class="fa fa-thumbs-down fa-2x text-gray-300"></i>
@@ -93,74 +79,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Content Row -->
-
-    <div class="row">
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
-
-            <!-- Page Heading -->
-            <h1 class="h3 mb-2 text-gray-800">Grafik Keanggotaan</h1>
-            <p class="mb-4">Grafik perkembangan jumlah anggota dan aktifitas komunitas PAKKEM.org</p>
-
-            <!-- Content Row -->
-            <div class="row">
-
-                <div class="col-xl-8 col-lg-7">
-
-                    <!-- Area Chart -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Jumlah Member Mendaftar Thn.
-                                <?= $tahun; ?></h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="chart-area">
-                                <canvas id="lineChart"></canvas>
-                            </div>
-                            <hr>
-                            Info detail member bisa dilihat di
-                            <a href="http://localhost/pakkem/admin/templates/daftar_anggota.php">/pakkem/admin/templates/daftar_anggota.php</a> di fitur kelola
-                            Admin.
-                        </div>
-                    </div>
-
-                    <!-- Bar Chart delete-->
-                </div>
-
-                <!-- Donut Chart -->
-                <div class="col-xl-4 col-lg-5">
-                    <div class="card shadow mb-2">
-                        <!-- Card Header - Dropdown -->
-                        <div class="card-header py-1">
-                            <h6 class="m-2 font-weight-bold text-primary">Rasio Member (%)</h6>
-                        </div>
-                        <!-- Card Body -->
-                        <div class="card-body">
-                            <div class="chart-area">
-                                <canvas id="doughnutChart" style="width:100% !important;"></canvas>
-                            </div>
-                            <hr>
-                            Info detail member bisa dilihat di
-                            <code>tabel-anggota/anggota-terdaftar/anggota-aktif</code> di fitur kelola
-                            Admin.
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <!-- /.container-fluid -->
-
-    </div>
-
-    <!-- Content Row -->
-    <div class="row">
-
-
-    </div>
-
 </div>
 <!-- /.container-fluid -->
 
--- member/templates/profil.php
+++ member/templates/profil.php
@@ -1,5 +1,11 @@
 <?php include "base/navbar.php"; ?>
-
-asasa
-
+<?php include "../models/member.php"; ?>
+<div class="container mx-auto">
+    <table class="table">
+        <tr>
+            <th>Nama</th>
+            <td>Yogga Aji Pratama</td>
+        </tr>
+    </table>
+</div>
 <?php include "base/footer.php"; ?>
