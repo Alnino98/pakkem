85b2b01dec4d542c691839f6065f977780b4b8ae
Moh Ramdani Yusman <moh.ramdani22@gmail.com>
2020-09-25 15:00:17 +0700

    Modul karak separo, urang di suruh momsky hela

--- admin/templates/modul_upload.php
+++ admin/templates/modul_upload.php
@@ -1,5 +1,55 @@
 <?php require "base/navbar.php" ?>
 
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
 
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Modul <a style="font-size: 10pt" href="tambah_modul.php" type="button" class="float-right btn btn-primary btn-lg">+ Upload Modul</a></h1>
 
+                    <table class="table small">
+                        <tr>
+                            <th>No</th>
+                            <th>ID Modul</th>
+                            <th>Nama Modul</th>
+                            <th>Keterangan</th>
+                            <th>Upload File</th>
+                            <th>Waktu Upload</th>
+                            <th>Opsi</th>
+
+                        </tr>
+
+                        <?php 
+                            include 'koneksi.php';
+                            $no = 1;
+                            $data = mysqli_query($koneksi,"select * from modul ORDER BY waktu_upload_modul ASC");
+                            while($d = mysqli_fetch_array($data)){
+                                ?>
+                                <tr>
+                                    <td><?php echo $no++; ?></td>
+                                    <td><?php echo $d['id_modul']; ?></td>
+                                    <td><?php echo $d['nama_modul']; ?></td>
+                                    <td><?php echo $d['keterangan_modul']; ?></td>
+                                    <td><?php echo $d['modul_pdf']; ?></td>
+                                    <td><?php echo $d['waktu_upload_modul']; ?></td>
+                                    
+                                    <td>
+                                        <a style="font-size: 10pt" href="edit.php?id_agenda=<?php echo $d['id_agenda']; ?>" type="button" class="btn btn-success">EDIT</a>
+                                        <a style="font-size: 10pt" href="hapus.php?id_agenda=<?php echo $d['id_agenda']; ?>&file='<?= $d['kegiatan_pdf'] ?>'" type="button" class="btn btn-danger">HAPUS</a>
+                                    </= $d['kegiatan_pdf']
+                                    </td>
+                                    
+
+                                </tr>
+                                <?php 
+                            }
+                            ?>
+                    </table>
+                     
+
+                </div>
+                <!-- /.container-fluid -->
+
+         
 <?php require "base/footer.php" ?>
+
+
---
+++ admin/templates/tambah_modul.php
@@ -0,0 +1,35 @@
+<?php require "base/navbar.php" ?>
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Daftar Anggota</h1>
+                    <form enctype="multipart/form-data" method="post" action="tambah_aksi_modul.php">
+                        <table class="table">
+
+                            <tr>            
+                                <td>Nama Modul</td>
+                                <td><input type="text" name="nama_modul"></td>
+                            </tr>
+                            <tr>
+                                <td>Keterangan</td>
+                                <td><input type="text" name="keterangan_modul"></td>
+                            </tr>
+                            <tr>
+                                <td>Upload file</td>
+                                <td>
+                                <input type="file" name="fupload1"><br>
+                                </td>
+
+                            </tr>
+  
+                            <tr>
+                                <td></td>
+                                <td><input type="submit" name="SIMPAN"></td>
+                            </tr>       
+                        </table>
+                    </form>
+
+
+                </div>
+                <!-- /.container-fluid -->
+<?php require "base/footer.php" ?>
