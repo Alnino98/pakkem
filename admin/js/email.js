0d256d6eaafd8531af3dabaf2b2a34d26e369fa3
yoggaajipratama <yoggaajipratama99@gmail.com>
2020-09-27 19:05:36 +0700

    fix

---
+++ admin/js/email.js
@@ -0,0 +1,12 @@
+setInterval(function(){
+    var xmlhttp = new XMLHttpRequest();
+    var response;
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            response = this.responseText;
+            document.getElementById("email").innerHTML = response;
+        }
+    }
+    xmlhttp.open("GET", "../models/email_model.php?id=1", true);
+    xmlhttp.send();
+}, 3000);
